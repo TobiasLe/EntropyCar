6e3edc160b19ec237b5d7a9143f0e73858aa01d1
Tobias <tobiasllemke@gmail.com>
2021-03-12 13:51:26 +0100

    new label

--- latex_label.py
+++ latex_label.py
@@ -13,8 +13,14 @@ from matplotlib.figure import Figure
 # fig.text(.5, .5, r'$\mathrm{ln}(\Omega)$', fontsize=40, color="white")
 # fig.savefig("ln(omega).svg", bbox_inches="tight", facecolor=(1,1,1,0))
 
+# fig = Figure(figsize=(5, 4), dpi=100)
+# canvas = FigureCanvasAgg(fig)
+#
+# fig.text(.5, .5, r'$\cdot10^6$', fontsize=40, color="white")
+# fig.savefig("mal10_6.svg", bbox_inches="tight", facecolor=(1,1,1,0))
+
 fig = Figure(figsize=(5, 4), dpi=100)
 canvas = FigureCanvasAgg(fig)
 
-fig.text(.5, .5, r'$\cdot10^6$', fontsize=40, color="white")
-fig.savefig("mal10_6.svg", bbox_inches="tight", facecolor=(1,1,1,0))
+fig.text(.5, .5, r'$\mathrm{S}=\mathrm{k_B} \mathrm{ln}(\Omega)$', fontsize=40, color="white")
+fig.savefig("entropy_equ.svg", bbox_inches="tight", facecolor=(1,1,1,0))
